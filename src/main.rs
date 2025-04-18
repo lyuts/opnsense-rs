4f50933e6c7e7b3107b529a27a5de23784d8b855
lyuts <lyuts@pm.me>
2025-04-18 19:47:09 -0400

    Implement parsing of API model.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+mod model;
+
+use model::{get_apis, Api};
+
+fn main() -> anyhow::Result<()> {
+    let apis: Vec<Api> = get_apis()?;
+    Ok(())
 }
---
+++ src/model.rs
@@ -0,0 +1,70 @@
+use nom::{
+    character::complete::{alpha1, alphanumeric1, char, space0},
+    combinator::{map, opt},
+    multi::separated_list0,
+    sequence::preceded,
+    IResult, Parser,
+};
+
+pub struct Api {
+    pub method: String,
+    pub module: String,
+    pub controller: String,
+    pub command: String,
+    pub parameters: Vec<String>,
+}
+
+fn parameter(input: &str) -> IResult<&str, &str> {
+    map(
+        (
+            char::<&str, nom::error::Error<&str>>('$'),
+            alphanumeric1,
+            opt(preceded(char('='), alphanumeric1)),
+        ),
+        |(_, p, _)| p.trim(),
+    )
+    .parse(input)
+}
+
+fn parameters(input: &str) -> IResult<&str, Vec<&str>> {
+    separated_list0(char(','), parameter).parse(input)
+}
+
+fn api_model(input: &str) -> IResult<&str, Api> {
+    map(
+        (
+            alpha1,
+            space0,
+            alpha1,
+            space0,
+            alpha1,
+            space0,
+            alpha1,
+            opt(space0),
+            opt(parameters),
+        ),
+        |(method, _, module, _, controller, _, command, _, parameters)| Api {
+            method: method.to_owned(),
+            module: module.to_owned(),
+            controller: controller.to_owned(),
+            command: command.to_owned(),
+            parameters: parameters
+                .unwrap_or_default()
+                .iter()
+                .map(ToString::to_string)
+                .collect(),
+        },
+    )
+    .parse(input)
+}
+
+pub fn get_apis() -> anyhow::Result<Vec<Api>> {
+    let model_content = include_str!("model.txt");
+
+    Ok(model_content
+        .split('\n')
+        .filter(|l| !l.trim().is_empty())
+        .map(|l| api_model(l).inspect_err(|e| eprintln!("Failed to parse line: {}", e)))
+        .map(|r| r.unwrap().1)
+        .collect::<Vec<Api>>())
+}
