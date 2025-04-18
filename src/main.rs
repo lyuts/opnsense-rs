e0f7f275c8fed6b5b70ad5de8d2f656eb7c9a1f0
lyuts <lyuts@pm.me>
2025-04-18 19:50:09 -0400

    Read ini file for API key.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 mod model;
 
+use anyhow::anyhow;
 use model::{get_apis, Api};
 
 fn main() -> anyhow::Result<()> {
-    let apis: Vec<Api> = get_apis()?;
+    let _apis: Vec<Api> = get_apis()?;
+
+    let conf = ini::Ini::load_from_file(".opn.ini")?;
+    let profile_data = conf.section(Some("default")).ok_or(anyhow!("Profile not found."))?;
+    let _key = profile_data.get("key").ok_or(anyhow!("key is missing in the profile."))?;
+    let _secret = profile_data.get("secret").ok_or(anyhow!("secret is missing in the profile."))?;
     Ok(())
 }
